ad8e1a1997db42ca8503ea34c19e27cac3df4862
Nur-ayn Hashim <nur-ayn.hashim@prodigyfinance.com>
2021-05-13 13:22:10 +0200

    fixed test_parallel_assignments, test_non_parallel_assignment, test_parallel_assignments_with_extra_values

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -3,19 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
@@ -38,7 +38,7 @@ class AboutArrayAssignment < Neo::Koan
 
   def test_parallel_assignment_with_one_variable
     first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    assert_equal ["John", "Smith"], first_name
   end
 
   def test_swapping_with_parallel_assignment
